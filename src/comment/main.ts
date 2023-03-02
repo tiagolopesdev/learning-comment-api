99b427de6b1b4cfd21b2be5bef72194b28360399
tiagoLopesAX <tiago.costa@ax4b.com>
2023-03-01 21:12:34 -0300

    Feat - settings to use swagger

---
+++ src/comment/main.ts
@@ -0,0 +1,21 @@
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { CommentModule } from './comment.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(CommentModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Comment API')
+    .setDescription('API used to study framework Nest js')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3000);
+  console.log(`API running on: PORT 3000`);
+
+}
+bootstrap();
