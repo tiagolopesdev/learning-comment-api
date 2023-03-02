ff420486eb40f64bf2842ac832912c2d5329ef70
tiagoLopesAX <tiago.costa@ax4b.com>
2023-03-01 21:49:52 -0300

    Feat - settings to use database mongoDb

--- src/comment/comment.module.ts
+++ src/comment/comment.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common';
-import { CommentService } from './comment.service';
-import { CommentController } from './comment.controller';
+import { CommentService } from './services/comment.service';
+import { CommentController } from './controllers/comment.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { CommentSchema } from './entities/comment.entity';
 
 @Module({
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://comment:j5OJZ1uorYP8kxD4@cluster0.ad379n6.mongodb.net/?retryWrites=true&w=majority'
+    ),
+    MongooseModule.forFeature([
+      {
+        name: 'comment',
+        schema: CommentSchema
+      }
+    ])
+  ],
   controllers: [CommentController],
   providers: [CommentService]
 })
--- src/comment/entities/comment.entity.ts
+++ src/comment/entities/comment.entity.ts
@@ -1,1 +1,16 @@
-export class Comment {}
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { HydratedDocument } from "mongoose";
+
+export type CommentDocument = HydratedDocument<Comment>;
+
+@Schema()
+export class Comment {
+    @Prop()
+    title: string;
+    @Prop()
+    body: string;
+    @Prop()
+    author: string;
+}
+
+export const CommentSchema = SchemaFactory.createForClass(Comment);
