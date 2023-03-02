af4e95d607852e6e3d1e60c3ad2a1450487c485e
tiagoLopesAX <tiago.costa@ax4b.com>
2023-03-01 21:54:00 -0300

    Feat - DTO's files created

--- src/comment/dto/create-comment.dto.ts
+++ src/comment/dto/create-comment.dto.ts
@@ -1,1 +1,26 @@
-export class CreateCommentDto {}
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateCommentDto {
+
+  id: string;
+  
+  @ApiProperty()
+  title: string;
+
+  @ApiProperty()
+  body: string;
+
+  @ApiProperty()
+  author: string;
+
+  private constructor(id: string, title: string, body: string, author: string) {
+    this.id = id;
+    this.title = title;
+    this.body = body;
+    this.author = author;
+  }
+
+  static convertAllElements (id: string, title: string, body: string, author: string) {
+    return new CreateCommentDto(id, title, body, author);
+  } 
+}
--- src/comment/dto/update-comment.dto.ts
+++ src/comment/dto/update-comment.dto.ts
@@ -1,4 +1,2 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateCommentDto } from './create-comment.dto';
 
-export class UpdateCommentDto extends PartialType(CreateCommentDto) {}
+export class UpdateCommentDto {}
